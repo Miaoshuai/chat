c90640a57539b36a63bf1818256fbb4fba3128b2
miaoshua <945970809@qq.com>
2015-11-24 19:37:26 +0800

    聊天服务器的实现

--- server.cpp
+++ server.cpp
@@ -25,15 +25,15 @@ class ChatServer : boost::noncopyable
     public:
         ChatServer(EventLoop *loop,const InetAddress &listenAddr)
         :server_(loop,listenAddr,"ChatServer"),
-        codec_(std::bind(&ChatServer::onStringMessage,this,_1,_2,_3))
+        codec_(std::bind(&ChatServer::onStringMessage,this,std::placeholders::_1,std::placeholders::_2,std::placeholders::_3))
         {
-            server_.setConnectionCallback(std::bind(&ChatServer::onStringMessage,this,_1));
-            server_.setMessageCallback(std::bind(&LengthHeaderCodec::onMessage,&codec_,_1,_2,_3));
+            server_.setConnectionCallback(std::bind(&ChatServer::onConnection,this,std::placeholders::_1));
+            server_.setMessageCallback(std::bind(&LengthHeaderCodec::onMessage,&codec_,std::placeholders::_1,std::placeholders::_2,std::placeholders::_3));
 
-            void start()
-            {
-                server_.start();
-            }
+        }
+        void start()
+        {
+            server_.start();
         }    
     private:
         //新连接或断开时的回调
@@ -78,7 +78,7 @@ int main(int argc,char **argv)
   if(argc > 2)
   {
       EventLoop loop;
-      InetAddress serverAddr(argv[1],static<uint16_t>(atoi(argv[2])));
+      InetAddress serverAddr(argv[1],static_cast<uint16_t>(atoi(argv[2])));
       ChatServer server(&loop,serverAddr);
       server.start();
       loop.loop();
